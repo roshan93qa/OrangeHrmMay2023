53649fe4a52b282ab63dc4060b02476b900411c8
Roshan Giri <111108258+roshan93qa@users.noreply.github.com>
2024-05-17 19:06:00 +0530

    Update RegistrationPageGit.java

--- src/test/java/com/qa/opencart/tests/RegistrationPageGit.java
+++ src/test/java/com/qa/opencart/tests/RegistrationPageGit.java
@@ -3,6 +3,7 @@ package com.qa.opencart.tests;
 public class RegistrationPageGit {
 	public static void main(String[] args) {
 		System.out.println("To the registration page of order page of values ");
+		System.out.println("To the registration page updated now this is pages order page of values ");
 	}
 
 }
